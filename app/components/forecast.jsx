2c235d37b0ce865dc0c563fe0371d6951d27d971
Christopher W. Brown <christopherwilliambrown@gmail.com>
2017-03-07 09:35:20 -0500

    More API buildout

--- app/components/forecast.jsx
+++ app/components/forecast.jsx
@@ -1,28 +1,46 @@
 var React = require('react');
 var WeatherForm = require('WeatherForm');
 var Message = require('Message');
+var WeatherMap = require('WeatherMap');
 
 var Forecast = React.createClass({
   getInitialState: function () {
     return {
-        location: 'Washington',
-        temp: 60
+      isLoading: false
     }
   },
   handleSearch: function (location) {
-    this.setState({
-      location: location,
-      temp: 25
+    var that = this;
+
+    this.setState({isLoading: true});
+
+    WeatherMap.getTemp(location).then(function (temp) {
+      that.setState({
+        location: location,
+        temp: temp,
+        isLoading: false
+      });
+    }, function (errorMessage) {
+      that.setState({isLoading: false});
+      alert(errorMessage);
     });
   },
   render: function () {
-    var {temp, location} = this.state;
+    var {isLoading, temp, location} = this.state;
+
+    function renderMessage () {
+      if (isLoading) {
+        return <h3>Getting the weather...</h3>;
+      } else if (temp && location) {
+        return <Message temp={temp} location={location}/>;
+      }
+    }
 
     return (
       <div>
-        <h3>Forecast</h3>
+        <h3>Weather Component</h3>
         <WeatherForm onSearch={this.handleSearch}/>
-        <Message temp={temp} location={location}/>
+        {renderMessage()}
       </div>
     )
   }
--- app/components/message.jsx
+++ app/components/message.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var Message = React.createClass ({
     render: function () {
-    var {temp, location} =this.props;
+    var {temp, location} = this.props;
     return (
       <h3>The temp is {temp} in {location} today.</h3>
     );
